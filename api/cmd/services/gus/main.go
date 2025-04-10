716008bea10d76c29f720c9883edeb5241dde216
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-04-10 11:36:53 -0400

    add main.go with signal chan

---
+++ api/cmd/services/gus/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/gradientsearch/gus/foundation/logger"
+)
+
+func main() {
+	l := logger.New(os.Stdout, logger.LevelInfo, "GUS", nil)
+	if err := run(context.Background(), l); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGINT)
+
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown complete", "sig", sig)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gradientsearch/gus
+
+go 1.23.3
